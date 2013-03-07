43c1baa45bb17c11e9937a7e7124e91647022289
fishappear <udit.agarwal@openlabs.co.in>
2013-03-07 17:39:57 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,2 @@
-# -*- coding: utf-8 -*-
-"""
-    __init__
 
-    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
-"""
-from trytond.pool import Pool
-from .party import *
-
-def register():
-    Pool.register(
-        Company,
-        Employee,
-        module='company', type_='model')
-
-from trytond.pool import Pool
-
-def register():
-    Pool.register(
-        module='payroll', type_='model')
 
